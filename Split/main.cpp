8ef1be22d942492abc25193303dbbc5c82ada3fa
Рафаэль Мухамедьяров <Rafael17IS16@gmail.com>
2023-04-09 12:45:21 +0300

    Modify split

--- Split/main.cpp
+++ Split/main.cpp
@@ -4,33 +4,29 @@ using namespace std;
 
 void Fill_Rand(int* arr, int size);
 void Print_Arr(int* arr, int size);
-void Fill_Even(int* arr, int* even, int size1, int& size2);
-void Fill_Odd(int* arr, int* even, int size1, int& size2);
+void Count_Even_Odd(int* arr, int size, int& count_even, int& count_odd);//подсчитаем кол-во четных и нечетных, это будут размеры массивов.
+void Split_Arr(int* arr, int* even, int* odd, int size);//делим массив на два - с четными и нечетными числами.
 
 void main()
 {
 	setlocale(LC_ALL, "");
 	int size;
-	int even_size = 0;
-	int odd_size = 0;
+	int count_even = 0, count_odd = 0;
 
 	cout << "Введите размер массива: "; cin >> size;
 	int* arr = new int[size];
 	Fill_Rand(arr, size);
 	Print_Arr(arr, size);
-	cout << "\n\n";
+	Count_Even_Odd(arr, size, count_even, count_odd);
 
-	int* even = new int[size];
-	Fill_Even(arr, even, size, even_size);
+	int* even = new int[count_even];
+	int* odd = new int[count_odd];
+
+	Split_Arr(arr, even, odd, size);
 	cout << "Массив с четными числами:" << endl;
-	Print_Arr(even, even_size);
-	
-	cout << "\n\n";
-
-	int* odd = new int[size];
-	Fill_Odd(arr, odd, size, odd_size);
+	Print_Arr(even, count_even);
 	cout << "Массив с нечетными числами:" << endl;
-	Print_Arr(odd, odd_size);
+	Print_Arr(odd, count_odd);
 
 	delete[] even;
 	delete[] odd;
@@ -48,27 +44,29 @@ void Print_Arr(int* arr, int size)
 {
 	for (int i = 0; i < size; i++)
 		cout << *(arr + i) << '\t';
+	cout << endl;
 }
 
-void Fill_Even(int* arr, int* even, int size1, int& size2)
+void Count_Even_Odd(int* arr, int size, int& count_even, int& count_odd)
 {
-	for (int i = 0; i < size1; i++)
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] % 2 == 0)
+			count_even++;
+		else
+			count_odd++;
+	}
+}
+
+void Split_Arr(int* arr, int* even, int* odd, int size1)
+{
+	for (int i = 0, j = 0, k = 0; i < size1; i++)
 	{
 		if ((arr[i] % 2) == 0)
-		{
-			even[size2++] = arr[i];
-		}
+			even[j++] = arr[i];
+		else
+			odd[k++] = arr[i];
 	}
 }
 
-void Fill_Odd(int* arr, int* odd, int size1, int& size2)
-{
-	for (int i = 0; i < size1; i++)
-	{
-		if (arr[i] % 2)
-		{
-			odd[size2++] = arr[i];
-		}
-	}
-}
 
